aa37f714f9f34330973f9703ebcfedb3cf80a9ee
Deniz Saganak <saganak.deniz@gmail.com>
2023-02-06 16:51:14 +0100

    add item component and display data

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { useState } from 'react';
 import { useQuery } from 'react-query';
 //components
+import Item from './Item/Item';
 import Drawer from '@material-ui/core/Drawer';
 import { LinearProgress } from '@material-ui/core';
-import Grid from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 import Badge from '@material-ui/core/Badge';
 //styles
-import { Wrapper } from './App.styles';
+import { Wrapper, StyledButton } from './App.styles';
+
+
 //types
 export type CartItemType = {
   id: number;
@@ -25,17 +28,47 @@ const getProducts = async (): Promise<CartItemType[]> => {
 
 
 function App() {
+  const [isCartOpen, setIsCartOpen] = useState(false);
+  const [cartItems, setCartItems] = useState([] as CartItemType[]);
+
   const { data, isLoading, error } = useQuery(
     'products',
     getProducts
     );
 
-  console.log(data);  
+  console.log(data); 
+  
+  const getTotalItems = (items: CartItemType[]) => {
+    return items.reduce((acc: number, item) => acc + item.amount, 0);
+  };
+
+  const handleAddToCart = (clickedItem: CartItemType) => null;
+
+  const handleRemoveFromCart = () => null;
+
+  if(isLoading) return <LinearProgress />;
+  if(error) return <div>Something went wrong...</div>
 
   return (
-    <div className="App">
-      Start
-    </div>
+    <Wrapper>
+      <Drawer anchor='right' open={isCartOpen} onClose={() => setIsCartOpen(false)} >
+        cart goes here
+      </Drawer>
+      <StyledButton onClick={() => setIsCartOpen(true)}>
+        <Badge badgeContent={getTotalItems(cartItems)} color="error">
+          <AddShoppingCartIcon />
+        </Badge>
+      </StyledButton>
+
+
+      <Grid container spacing={3}>
+        {data?.map(item => (
+          <Grid item key={item.id} xs={12} sm={4}>
+            <Item item={item} handleAddToCart={handleAddToCart} />
+          </Grid>
+        ))}
+      </Grid>
+    </Wrapper>
   );
 }
 
